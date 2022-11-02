246cc72e262bd7613cd6061891977b72720a900d
dario <dacrumau@gmail.com>
2022-11-02 09:08:00 +0100

    trad

--- sale_additional_fields/models/res_partner.py
+++ sale_additional_fields/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class ResPartner(models.Model):
@@ -11,21 +11,4 @@ class ResPartner(models.Model):
         inverse_name='partner_id',
         string="Licence",
         required=False,
-    )
-
-class LicenceCarPartner(models.Model):
-    _name = 'licence.car.partner'
-    _description = 'Licence Car Partner'
-
-    name = fields.Char(
-        string="Licence Car Partner",
-        required=True,
-        index=True,
-    )
-    partner_id = fields.Many2one(
-        comodel_name='partner',
-        string='partner',
-        required=True,
-        ondelete='cascade',
-        index=True,
     )

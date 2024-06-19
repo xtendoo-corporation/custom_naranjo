1e446446670bced5946138f0a174073c1a33d8f3
Gokac9 <theabraham9@gmail.com>
2024-06-19 12:34:47 +0200

    num ip value correction in rectified invoice

--- sale_additional_fields/models/account_move_line.py
+++ sale_additional_fields/models/account_move_line.py
@@ -7,23 +7,33 @@ from odoo import _, api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-
-    def _compute_ip_number(self):
-        self.ip_number = ""
-        if self.move_id.move_type == "out_invoice":
-            for line in self.filtered("sale_line_ids"):
-                for sale_line in line.sale_line_ids:
-                    if sale_line.ip_number:
-                        line.ip_number = sale_line.ip_number
-
-        elif self.move_id.move_type == "in_invoice":
-            for line in self.filtered("purchase_order_id"):
-               line.ip_number = line.purchase_order_id.ip_number
+    # def _compute_ip_number(self):
+    #     self.ip_number = ""
+    #     if self.move_id.move_type == "out_invoice":
+    #         for line in self.filtered("sale_line_ids"):
+    #             for sale_line in line.sale_line_ids:
+    #                 if sale_line.ip_number:
+    #                     line.ip_number = sale_line.ip_number
+    #
+    #     elif self.move_id.move_type == "in_invoice":
+    #         for line in self.filtered("purchase_order_id"):
+    #             line.ip_number = line.purchase_order_id.ip_number
 
     ip_number = fields.Char(
         string='IP number',
         compute='_compute_ip_number',
     )
 
+    @api.depends('move_id.move_type', 'sale_line_ids.ip_number', 'purchase_order_id.ip_number')
+    def _compute_ip_number(self):
+        for record in self:
+            record.ip_number = ""
+            if record.move_id.move_type in ["out_invoice", "out_refund"]:
+                for line in record.filtered("sale_line_ids"):
+                    for sale_line in line.sale_line_ids:
+                        if sale_line.ip_number:
+                            record.ip_number = sale_line.ip_number
 
-
+            elif record.move_id.move_type in ["in_invoice", "in_refund"]:
+                for line in record.filtered("purchase_order_id"):
+                    record.ip_number = line.purchase_order_id.ip_number
